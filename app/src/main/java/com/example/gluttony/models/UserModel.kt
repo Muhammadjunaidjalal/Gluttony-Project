19f9c5f4a1c61667d9cbd64f9f5054adf52f953e
JUNAID <Mrjunaidjalallive@gmail.com>
2024-07-23 17:58:15 +0500

    Commit For Models Customer Gluttony

---
+++ app/src/main/java/com/example/gluttony/models/UserModel.kt
@@ -0,0 +1,9 @@
+package com.example.gluttony.models
+
+data class UserModel(val userName :String? = null,
+                     val password : String? = null,
+                     val email:String?= null,
+                     val address:String?= null,
+                     val phone:String?= null
+
+)
